6c67590a768eb3a5ffdc4a81424a8a93fb1b58da
mmsys34 <leon@more.markets>
2025-03-20 06:05:22 -0300

    feat: add sanction model for restricted regions

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -9,3 +9,5 @@ export const multicalls = {
   testnet: '0xF7d11c74B5706155d7C6DBe931d590611a371a8a',
   mainnet: '0x8358d18E99F44E39ea90339c4d6E8C36101f8161',
 };
+
+export const sanctionedCountries = ["KP", "IR", "MM", "US"];
