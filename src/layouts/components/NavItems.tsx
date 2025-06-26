84aecc5541323b3a70d313be2e85d20fbdbe29ba
Tupac <7931085+1tupac@users.noreply.github.com>
2025-06-26 10:57:14 +0930

    feat: make vault public

--- src/layouts/components/NavItems.tsx
+++ src/layouts/components/NavItems.tsx
@@ -42,7 +42,6 @@ export const NavItems = ({ setOpen }: NavItemsProps) => {
       title: 'Vaults',
       visibleTitle: 'Vaults',
       dataCy: 'menuVaults',
-      isVisible: () => ENABLE_TESTNET,
     },
     {
       link: ROUTES.staking,
