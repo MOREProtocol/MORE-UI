f63f907bada2dcadd49119234502d507b1a3c3e2
Tupac <7931085+1tupac@users.noreply.github.com>
2025-07-02 10:43:38 +0930

    feat: update wagmi config

--- src/utils/wagmi.ts
+++ src/utils/wagmi.ts
@@ -1,11 +1,12 @@
 import { getDefaultConfig } from 'connectkit';
-import { createConfig } from 'wagmi';
+import { createConfig, fallback, http } from 'wagmi';
 import { flowMainnet, flowTestnet } from 'viem/chains';
 
 const connectKitConfig = getDefaultConfig({
-  appName: 'MORE Markets',
+  appName: 'MORE',
+  appDescription: 'Integrate with any DeFi protocol',
   appUrl: 'https://app.more.markets',
-  appIcon: '/loveMore.svg',
+  appIcon: 'https://avatars.githubusercontent.com/u/208097089?s=200&v=4',
 
   // WalletConnect Project ID (required)
   walletConnectProjectId: process.env.NEXT_PUBLIC_WALLET_CONNECT_PROJECT_ID!,
@@ -16,7 +17,12 @@ const connectKitConfig = getDefaultConfig({
   // Supported chains
   chains: [flowMainnet, flowTestnet],
 
+  transports: {
+    [flowMainnet.id]: http(flowMainnet.rpcUrls.default.http[0]),
+    [flowTestnet.id]: http(flowTestnet.rpcUrls.default.http[0]),
+  },
+
   ssr: true
 });
-export const config = createConfig(connectKitConfig);
 
+export const config = createConfig(connectKitConfig);
