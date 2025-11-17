36234cb9150546265b89773b23620f4dc2eb39e9
1tupac <7931085+1tupac@users.noreply.github.com>
2025-11-17 17:26:07 +0100

    feat: add supply cap handling

--- src/modules/reserve-overview/SupplyInfo.tsx
+++ src/modules/reserve-overview/SupplyInfo.tsx
@@ -1,14 +1,10 @@
-import { valueToBigNumber } from '@aave/math-utils';
 import { Box, Typography } from '@mui/material';
-import { CapsCircularStatus } from 'src/components/caps/CapsCircularStatus';
 import { FormattedNumber } from 'src/components/primitives/FormattedNumber';
-import { Link } from 'src/components/primitives/Link';
 import { ReserveSubheader } from 'src/components/ReserveSubheader';
 import { TextWithTooltip } from 'src/components/TextWithTooltip';
 import { ComputedReserveData } from 'src/hooks/app-data-provider/useAppDataProvider';
 import { AssetCapHookData } from 'src/hooks/useAssetCaps';
 import { MarketDataType } from 'src/utils/marketsAndNetworksConfig';
-import { GENERAL } from 'src/utils/mixPanelEvents';
 import { PoolReservesRewardsHumanized, usePoolReservesRewardsHumanized } from 'src/hooks/pool/usePoolReservesRewards';
 
 import { PanelItem } from './ReservePanels';
@@ -30,6 +26,10 @@ export const SupplyInfo = ({
   showSupplyCapStatus,
   supplyCap,
 }: SupplyInfoProps) => {
+  console.log('supplyCap', supplyCap);
+  console.log('reserve', reserve);
+  console.log('currentMarketData', currentMarketData);
+  console.log('showSupplyCapStatus', showSupplyCapStatus);
   const rewardsQuery = usePoolReservesRewardsHumanized(currentMarketData);
   const allRewards: PoolReservesRewardsHumanized[] = rewardsQuery?.data ?? [];
   const supplyRewards = useMemo(() => allRewards.filter(
@@ -48,105 +48,36 @@ export const SupplyInfo = ({
         backgroundColor: 'background.paper',
         borderRadius: 2,
       }}>
-        {showSupplyCapStatus ? (
-          // With supply cap
-          <>
-            <CapsCircularStatus
-              value={supplyCap.percentUsed}
-              tooltipContent={
-                <>
-                  {'Maximum amount available to supply is '}
-                  <FormattedNumber
-                    value={
-                      valueToBigNumber(reserve.supplyCap).toNumber() -
-                      valueToBigNumber(reserve.totalLiquidity).toNumber()
-                    }
-                    variant="secondary12"
-                  />{' '}
-                  {reserve.symbol} (
-                  <FormattedNumber
-                    value={
-                      valueToBigNumber(reserve.supplyCapUSD).toNumber() -
-                      valueToBigNumber(reserve.totalLiquidityUSD).toNumber()
-                    }
-                    variant="secondary12"
-                    symbol="USD"
-                  />
-                  ).
-                </>
-              }
-            />
-            <PanelItem
-              title={
-                <Box display="flex" alignItems="center">
-                  Total supplied
-                  <TextWithTooltip
-                    event={{
-                      eventName: GENERAL.TOOL_TIP,
-                      eventParams: {
-                        tooltip: 'Total Supply',
-                        asset: reserve.underlyingAsset,
-                        assetName: reserve.name,
-                      },
-                    }}
-                  >
-                    <>
-                      {
-                        'Asset supply is limited to a certain amount to reduce protocol exposure to the asset and to help manage risks involved. '
-                      }
-                      <Link
-                        href="https://docs.more.markets/developers/whats-new/supply-borrow-caps"
-                        underline="always"
-                      >
-                        Learn more
-                      </Link>
-                    </>
-                  </TextWithTooltip>
-                </Box>
-              }
-            >
-              <Box>
-                <FormattedNumber value={reserve.totalLiquidity} variant="main16" compact />
-                <Typography
-                  component="span"
-                  color="text.primary"
-                  variant="secondary16"
-                  sx={{ display: 'inline-block', mx: 1 }}
-                >
-                  of
-                </Typography>
-                <FormattedNumber value={reserve.supplyCap} variant="main16" />
-              </Box>
-              <Box>
-                <ReserveSubheader value={reserve.totalLiquidityUSD} />
-                <Typography
-                  component="span"
-                  color="text.secondary"
-                  variant="secondary12"
-                  sx={{ display: 'inline-block', mx: 1 }}
-                >
-                  of
-                </Typography>
-                <ReserveSubheader value={reserve.supplyCapUSD} />
-              </Box>
-            </PanelItem>
-          </>
-        ) : (
-          // Without supply cap
-          <Box>
+        <Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
             <Typography variant="secondary14" color="text.secondary">
               Total supplied
             </Typography>
-            <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: 1, alignItems: 'center' }}>
-              <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-                <FormattedNumber value={reserve.totalLiquidity} variant="main16" compact />
-              </Box>
-              <UsdChip
-                value={reserve.totalLiquidityUSD}
-              />
-            </Box>
+            {showSupplyCapStatus && (
+              <TextWithTooltip iconMargin={0.5}>
+                <>
+                  <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+                    <Typography variant="secondary12" color="text.secondary">
+                      Supply cap
+                    </Typography>
+                    <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                      <FormattedNumber value={reserve.supplyCap} variant="main14" />
+                      <UsdChip value={reserve.supplyCapUSD} />
+                    </Box>
+                  </Box>
+                </>
+              </TextWithTooltip>
+            )}
           </Box>
-        )}
+          <Box sx={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap', gap: 1, alignItems: 'center' }}>
+            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+              <FormattedNumber value={reserve.totalLiquidity} variant="main16" compact />
+            </Box>
+            <UsdChip
+              value={reserve.totalLiquidityUSD}
+            />
+          </Box>
+        </Box>
         <Box>
           <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
             <Typography variant="secondary14" color="text.secondary">
