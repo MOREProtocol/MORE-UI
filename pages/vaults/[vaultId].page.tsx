29d86d9b07eb96811120c503285527e8b5f82c01
1tupac <7931085+1tupac@users.noreply.github.com>
2026-01-06 12:36:23 +0100

    feat: add warning banner

--- pages/vaults/[vaultId].page.tsx
+++ pages/vaults/[vaultId].page.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material';
+import { Alert, Box } from '@mui/material';
 import { ReactNode } from 'react';
 import { MainLayout } from 'src/layouts/MainLayout';
 import { VaultDetail } from 'src/modules/vault-detail/VaultDetail';
@@ -24,6 +24,9 @@ const ContentContainer = ({ children }: ContentContainerProps) => {
 export default function VaultDetailPage() {
   return (
     <ContentContainer>
+      <Alert severity="warning" sx={{ borderRadius: '18px', m: 2 }}>
+        On Dec. 27, the Flow network experienced a security incident affecting the Cadence. Network recovery and reindexing are currently in progress. As a result, data displayed in MORE, especially Subgraph data such as APY, may be inaccurate.
+      </Alert>
       <VaultDetail />
     </ContentContainer>
   );
--- pages/vaults/index.page.tsx
+++ pages/vaults/index.page.tsx
@@ -1,3 +1,4 @@
+import { Alert } from '@mui/material';
 import { useEffect } from 'react';
 import { MainLayout } from 'src/layouts/MainLayout';
 import { VaultAssetsListContainer } from 'src/modules/vaults/VaultAssetsListContainer';
@@ -12,7 +13,12 @@ export default function Vaults() {
     });
   }, [trackEvent]);
   return (
-    <VaultAssetsListContainer />
+    <>
+      <Alert severity="warning" sx={{ borderRadius: '18px', m: 2 }}>
+        On Dec. 27, the Flow network experienced a security incident affecting the Cadence. Network recovery and reindexing are currently in progress. As a result, data displayed in MORE, especially Subgraph data such as APY, may be inaccurate.
+      </Alert>
+      <VaultAssetsListContainer />
+    </>
   );
 }
 
